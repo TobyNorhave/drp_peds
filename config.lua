3d0e6fd27e00aa57e1edb833f93b99450baae6e0
Darkzy <tomames360@hotmail.co.uk>
2020-05-19 16:09:28 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 DRPPeds = {}
-
 DRPPeds.PedLoc = {}
+
 DRPPeds.PedLoc = {
-    {ishash=true, model= 0xe497bbef, dict="mini@strip_club@idles@bouncer@base", anim="base", voice="S_M_M_HAIRDRESSER_01_BLACK_MINI_01", coords={-1645.63,-971.08,7.69,3.45}}, -- -1645.63, y =-971.08, z = 7.69, h = 3.45 
+    {ishash=true, model= "g_m_y_armgoon_02", dict="mini@strip_club@idles@bouncer@base", anim="base", voice="S_M_M_HAIRDRESSER_01_BLACK_MINI_01", coords={x = -1645.5, y = -986.61, z = 7.33, h = 4.18}}, -- -1645.63, y =-971.08, z = 7.69, h = 3.45
 }
