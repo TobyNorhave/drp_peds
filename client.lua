4cafa249474f8b8509cfeeb6f6d889c723d72cbb
Darkzy <tomames360@hotmail.co.uk>
2020-05-19 16:09:00 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,36 +1,35 @@
---local peds = {}
-local peds = {
-    {ishash=true, model= 0xe497bbef, dict="mini@strip_club@idles@bouncer@base", anim="base", voice="S_M_M_HAIRDRESSER_01_BLACK_MINI_01", coords={-1645.5,-986.61,7.33,4.18}}, -- -1645.63, y =-971.08, z = 7.69, h = 3.45
-}
-
--- Citizen.CreateThread(function()
---     TriggerServerEvent("DRP_Peds:InitAll")
--- end)
-
--- RegisterNetEvent("DRP_Peds:Init")
--- AddEventHandler("DRP_Peds:Init", function(pedsS)
---     peds = pedsS
--- end)
-
-
 Citizen.CreateThread(function()
-    for k,v in pairs(peds) do
-        local lmodel = (v.ishash) and v.model or GetHashKey(v.model)
+    TriggerServerEvent("DRP_Peds:InitAll")
+end)
+
+RegisterNetEvent("DRP_Peds:Init")
+AddEventHandler("DRP_Peds:Init", function(peds)
+    spawnPeds(peds)
+end)
+
+function deleteOldPeds()
+
+end
+
+function spawnPeds(peds)
+    for a = 1, #peds, 1 do
+        local lmodel = GetHashKey(peds[a].model)
         RequestModel(lmodel)
         while not HasModelLoaded(lmodel) do
             Wait(10)
         end
-        local lPed = CreatePed(4, lmodel, v.coords[1], v.coords[2], v.coords[3] - 1.0, v.coords[4], false, false)
+        local lPed = CreatePed(4, lmodel, peds[a].coords.x, peds[a].coords.y, peds[a].coords.z - 0.95, peds[a].coords.h, false, false)
+
         SetEntityInvincible(lPed, true)
         FreezeEntityPosition(lPed, true)
         SetBlockingOfNonTemporaryEvents(lPed, true)
-        SetAmbientVoiceName(lPed, v.voice)
-        if v.dict ~= nil then
-            loadAnimDict(v.dict)
-            TaskPlayAnim(lPed, v.dict, v.anim, 8.0, 0.0, -1, 1, 0, 0, 0, 0)
+        SetAmbientVoiceName(lPed, peds[a].voice)
+        if peds[a].dict ~= nil then
+            -- loadAnimDict("mini@strip_club@idles@bouncer@base")
+            -- TaskPlayAnim(lPed, peds[a].dict, peds[a].anim, 8.0, 0.0, -1, 1, 0, 0, 0, 0)
         else
             TaskStartScenarioInPlace(lPed, "WORLD_HUMAN_STAND_IMPATIENT_UPRIGHT", 0, 0)
         end
         SetModelAsNoLongerNeeded(lmodel)
     end
-end)
+end
